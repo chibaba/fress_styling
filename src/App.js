8ecee031efaf63e241bafce6152df41c2d218c13
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-02-14 17:03:48 +0100

    created new todo to application

--- src/App.js
+++ src/App.js
@@ -8,25 +8,54 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      userName : "Senior"
+      userName : "Senior",
+      todoItems: [{action: "Study Java how to program", done: false},
+                   {action: "Reduce the level of un-re-usable methods", done: false},
+                  {action: "Ensure you take your coffee", done: false},
+                {action: "Wash Clothes", done : false}],
+                nextItem: ""
     }
   }
 
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === "Senior" ? "Developer" : "Senior"
-    })
-  }
+   updateNewTextValue = (event) => {
+     this.setState({ newItemText: event.target.value });
+   }
+   createNewTodo = () => {
+     If(!this.state.todoItems
+          .find(item => item.action === this.state.newItemText)) {
+            this.setState({
+              todoItems: [...this.state.todoItems,
+              { action: this.state.newItemText, done: false }],
+              newItemText: ""
+            });
+          }
+   }
+
+  // changeStateData = () => {
+  //   this.setState({
+  //     userName: this.state.userName === "Senior" ? "Developer" : "Senior"
+  //   })
+ // }
   render() {
     return (
       <div>
         <h1 className = "bg-primary text-white text-center p-1">
           {this.state.userName}'s To Do LIST
+          ({ this.state.todoItems.filter(t => !t.done).length} items to do)
         </h1>
-        <button className="btn btn-primary m-2"
+        {/* <button className="btn btn-primary m-2"
         onClick={ this.changeStateData }>
           flip it
-        </button>
+        </button> */}
+        <div className="container-fluid">
+          <div className="my-1">
+            <input className="form-control"
+            value={ this.state.newItemText}
+            onChange={ this.updateNewTextValue} />
+            <button className="btn btn-primary mt-1"
+            onClick={ this.createNewTodo}>Add</button>
+          </div>
+        </div>
       </div>
     )
   }
